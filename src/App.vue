a1cea269de5d23f7952bce298eebc0f310921ad6
sirwilliamthefirst <30707549+sirwilliamthefirst@users.noreply.github.com>
2025-11-21 01:08:49 -0500

    added sliding drawer

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div class="desktop-header"><Header></Header></div>
+  <Header></Header>
 
   <div class="pond_container">
     <Pond></Pond>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,25 @@
 import { ref } from 'vue'
 
 const isMenuOpen = ref(false)
-
+const menuWidth = ref(0)
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
+  menuWidth.value = isMenuOpen.value ? 10 : 0
 }
 </script>
 
 <template>
+  <div v-if="isMenuOpen" class="backdrop" @click.stop()="isMenuOpen = false"></div>
+
+  <div id="mySidenav" class="sidenav" :class="{ open: isMenuOpen }">
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+  </div>
+
   <svg
+    v-if="!isMenuOpen"
     @click="toggleMenu"
     class="hamburger-icon"
     viewBox="0 0 24 24"
@@ -27,6 +38,16 @@ const toggleMenu = () => {
 </template>
 
 <style>
+.backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 1; /* Below sidenav but above content */
+}
+
 .hamburger-icon {
   position: fixed;
   top: 0;
@@ -35,6 +56,60 @@ const toggleMenu = () => {
   z-index: 1;
 }
 .hamburger-icon:hover path {
-  stroke: #ffffff; /* or whatever color you want on hover */
+  stroke: #ffffff;
+}
+.sidenav {
+  height: 100%; /* 100% Full-height */
+  opacity: 0;
+  width: 0;
+  background: #e8efb2;
+  border-left: 1px groove #00d4ff;
+  position: fixed; /* Stay in place */
+  z-index: 2; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 100%;
+  transform: translate(-100%, 0%);
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidenav */
+}
+
+.sidenav.open {
+  width: clamp(30px, 25vw, 150px);
+  opacity: 1;
+}
+
+/* The navigation menu links */
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+/* Position and style the close button (top right corner) */
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidenav {
+    padding-top: 15px;
+  }
+  .sidenav a {
+    font-size: 18px;
+  }
 }
 </style>
